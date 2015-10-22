72206d7ced4f579f10f4411f77f0c727a9d5eaa9
Jon Banafato <jon@jonafato.com>
2015-10-22 12:09:55 -0400

    Bump version 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='Henson-SQS',
-    version='0.1.0',
+    version='0.2.0',
     packages=find_packages(exclude=['tests']),
     install_requires=[
         'boto3>=1.1.1',
