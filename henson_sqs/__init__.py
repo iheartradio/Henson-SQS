cb3b2c897d5e2b528b9f37980c9f86999d199139
Jon Banafato <jon@jonafato.com>
2015-12-22 14:23:35 -0500

    Docstring cleanup

--- henson_sqs/__init__.py
+++ henson_sqs/__init__.py
@@ -19,28 +19,30 @@ class Consumer:
 
     For each message consumed from the queue, the specified callback
     will be called.
+
+    Args:
+        client (boto3.SQS.Client): The SQS client that should be used
+            for connections.
+        queue_url (str): The SQS queue URL to read from.
+        attribute_names (List[str]): Metadata attributes to return with
+            each SQS message.
+        message_attribtue_names (List[str]): Attributes of each message
+            to be returned.
+        message_batch_size (int): The maximum number of messages to be
+            retrieved with each call.
+        visibility_timeout (int): The number of seconds to hide the
+            returned messages from subsequent retrieve queries.
+        wait_time (int): The maximum amount of time to wait for new
+            messages.
+        delete_messages_on_read (Optional[bool]): Whether or not to
+            delete messages after they're read from the specified queue.
+            Defaults to ``True``.
     """
 
     def __init__(self, client, queue_url, attribute_names, message_attributes,
                  message_batch_size, visibility_timeout, wait_time,
                  delete_messages_on_read=True):
-        """Initialize the consumer.
-
-        Args:
-            client (:class:`~boto3.SQS.Client`): the SQS client that
-                should be used for connections
-            queue_url (str): the SQS queue URL to read from
-            attribute_names (List[str]): metadata attributes to return
-                with each SQS message
-            message_attribtue_names (List[str]): attributes of each
-                message to be returned
-            message_batch_size (int): the maximum number of messages to
-                be retrieved with each call
-            visibility_timeout (int): the number of seconds to hide
-                the returned messages from subsequent retrieve queries
-            wait_time (int): the maximum amount of time to wait for
-                new messages
-        """
+        """Initialize the consumer."""
         self.client = client
         self.queue_url = queue_url
         self.attribute_names = attribute_names
@@ -86,10 +88,10 @@ class Producer:
     """A producer to write to an SQS queue.
 
     Args:
-        client (:class:~boto3.SQS.Client): The client with which to
-          write messages.
+        client (boto3.SQS.Client): The client with which to write
+            messages.
         queue_url (str): The URL representing the SQS queue to which the
-          message should be written.
+            message should be written.
     """
 
     def __init__(self, client, queue_url):
@@ -122,9 +124,9 @@ class SQS(Extension):
     """An interface to interact with an SQS queue.
 
     Args:
-        app (optional): An application instance that has an attribute
-          named settings that contains a mapping of settings to interact
-          with SQS.
+        app (Optional[henson.base.Application): An application instance
+            that has an attribute named settings that contains a mapping
+            of settings to interact with SQS.
     """
 
     DEFAULT_SETTINGS = {
@@ -149,7 +151,7 @@ class SQS(Extension):
         """Return the connection to SQS.
 
         Returns:
-            :class:`~boto3.SQS.Client`: A connection to the SQS service.
+            boto3.SQS.Client: A connection to the SQS service.
         """
         session = Session(
             aws_access_key_id=self.app.settings['AWS_ACCESS_KEY'],
@@ -162,7 +164,7 @@ class SQS(Extension):
         """Return a new SQS consumer.
 
         Returns:
-            :class:`Consumer`: The new consumer.
+            Consumer: The new consumer.
         """
         settings = self.app.settings
         return Consumer(
@@ -180,7 +182,7 @@ class SQS(Extension):
         """Return a new SQS producer.
 
         Returns:
-            :class:`Producer`: The new producer.
+            Producer: The new producer.
         """
         return Producer(
             client=self.client,
